39e68e2ffba0296d2b57554c492757627541908d
Maria Falk <mariacelf@gmail.com>
2024-11-03 22:43:10 +0100

    Started buildning App.jsx with the compontents I've created so far.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import Brands from "./components/Brands";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="full-width-hero-bg">
-        <div className="wrapper flex-column"></div>
+        <div className="wrapper flex-column">
+          <Header />
+        </div>
       </div>
-
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <main class="main flex-column gap-5">
+          <Brands />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-
-import Nav from "./Nav"
 import Hero from "./Hero"
+import Nav from "./Nav"
+
 
 
 const Header = () => {
