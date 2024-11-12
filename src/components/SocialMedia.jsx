db79dc2cac5fa2ce9caf874d27e592fbe32b1e5b
Maria Falk <mariacelf@gmail.com>
2024-11-12 16:37:25 +0100

    made the socialmedia links better

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,36 +1,22 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const SocialMedia = () => {
   return (
     <div className="flex-left gap-1 mt-3 social-media">
-      <NavLink  to="https://facebook.com" className="icon-box"><i className="fa-brands fa-facebook"></i></NavLink>
-      <NavLink to="https://twitter.com" className="icon-box"><i className="fa-brands fa-twitter"></i></NavLink>
-      <NavLink  to="https://instagram.com" className="icon-box"><i className="fa-brands fa-square-instagram"></i></NavLink>
-      <NavLink  to="https://youtube.com" className="icon-box"><i className="fa-brands fa-youtube"></i></NavLink>
+      <NavLink to="https://facebook.com" target="_blank" rel="noopener noreferrer" className="icon-box">
+        <i className="fa-brands fa-facebook"></i>
+      </NavLink>
+      <NavLink to="https://twitter.com" target="_blank" rel="noopener noreferrer" className="icon-box">
+        <i className="fa-brands fa-twitter"></i>
+      </NavLink>
+      <NavLink to="https://instagram.com" target="_blank" rel="noopener noreferrer" className="icon-box">
+        <i className="fa-brands fa-square-instagram"></i>
+      </NavLink>
+      <NavLink to="https://youtube.com" target="_blank" rel="noopener noreferrer" className="icon-box">
+        <i className="fa-brands fa-youtube"></i>
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default SocialMedia;
