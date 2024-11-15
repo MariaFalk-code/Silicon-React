4b4b257e71a75abaa6fdcdf50f3faae858490df1
Maria Falk <mariacelf@gmail.com>
2024-11-15 16:37:34 +0100

    started working on the contactform

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className='form-box'>
+      <h2>Get Online Consultation</h2>
+      <form>
+        <div className='form-group'>
+          <label for='name'>Full name</label>
+          <input type='text' id='name' name='name' required />
+        </div>
+        <div className='form-group'>
+          <label for='email'>Email adress</label>
+          <input type='email' id='email' name='email' required />
+        </div>
+        <div className='form-group'>
+          <label for='specialist'>Specialist</label>
+          <select id='droplist' name='droplist'>
+            <option value='general'>General</option>
+            <option value='nurse'>Nurse</option>
+            <option value='doctor'>Doctor</option>
+            <option value='Physiotherapist'>Physioterapist</option>
+            <option value='psychiatrist'>Psychiatrist</option>
+          </select>
+        </div>
+        <button type='submit'>Make an appoinment</button>
+      </form>
+    
+    
+    
+    </div>
+  )
+}
+
+export default ContactForm
