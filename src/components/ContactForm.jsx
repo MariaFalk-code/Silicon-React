8cb1ef5326d411e9580a6ca1986ead55d8037ca8
Maria Falk <mariacelf@gmail.com>
2024-11-16 00:33:29 +0100

    Fixed the form so that the dropdown works in darkmode too

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,36 +1,50 @@
-
+import { useState } from "react";
+import Dropdown from "./Dropdown";
 
 const ContactForm = () => {
-  return (
-    <div className='form-box'>
-      <h2>Get Online Consultation</h2>
-      <form>
-        <div className='form-group'>
-          <label for='name'>Full name</label>
-          <input type='text' id='name' name='name' required />
-        </div>
-        <div className='form-group'>
-          <label for='email'>Email adress</label>
-          <input type='email' id='email' name='email' required />
-        </div>
-        <div className='form-group'>
-          <label for='specialist'>Specialist</label>
-          <select id='droplist' name='droplist'>
-            <option value=''>Select a specialist</option>
-            <option value='general'>General</option>
-            <option value='nurse'>Nurse</option>
-            <option value='doctor'>Doctor</option>
-            <option value='Physiotherapist'>Physioterapist</option>
-            <option value='psychiatrist'>Psychiatrist</option>
-          </select>
-        </div>
-        <button className="btn btn-l" type='submit'>Make an appoinment</button>
-      </form>
-    
-    
-    
-    </div>
-  )
-}
+  const [selectedSpecialist, setSelectedSpecialist] = useState("");
 
-export default ContactForm
+  const handleSelect = (option) => {
+    setSelectedSpecialist(option);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.append("specialist", selectedSpecialist);
+    const data = Object.fromEntries(formData);
+  };
+
+  return (
+    <div className="form-box">
+      <h2>Get Online Consultation</h2>
+      <form noValidate>
+        <div className="form-group">
+          <label htmlFor="name">Full name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email adress</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="specialist">Specialist</label>
+          <Dropdown
+            options={["Nurse", "Doctor", "Physiotherapist", "Psychiatrist"]}
+            placeholder={
+              <span className="arrow-gap">
+                Select a specialist <i className="fa-solid fa-caret-down"></i>
+              </span>
+            }
+            onSelect={handleSelect}
+          />
+        </div>
+        <button className="btn btn-l" type="submit">
+          Make an appoinment
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
